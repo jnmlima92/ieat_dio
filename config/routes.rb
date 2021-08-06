034d9cc16769c03d82f29a532fa317bbcc169c42
Joao Moura <jnmlima92@gmail.com>
2021-08-06 15:37:22 -0300

    Creating Product and Restaurant entity and your routines

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users
+  get 'products/index'
   root 'home#index'
+  devise_for :users
+
+  resources :restaurants
+  resources :products
 
   get 'home/index'
 end
